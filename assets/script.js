b5e37217dd8e6fc1b0bfb2a2d432b6852c563c38
Jerry Plotke <jerry.plotke@gmail.com>
2021-09-10 11:16:08 -0500

    new file structure

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,6 @@
-// window.onload = function() {
-
-//     alert("welcome");
-
-// };
-// $(document).ready(function() {
-
-//     $("a").click(function(event) {
-
-//         alert("Thanks for visiting!");
-
-//     });
-
 var searchBtnEl = document.querySelector("#enterBtn");
 var cityInput = document.querySelector("#city-input");
 var cityListEl = document.querySelectorAll('#city-list');
-
 var weatherFormEl = document.querySelectorAll('#weather-form');
 
 function showCityList(event) {
@@ -30,13 +16,31 @@ function showCityList(event) {
     cityListEl.append('<li>' + cityItem + '</li>');
 
     querySelectorAll('input[name="city-input"]').val('');
-
 }
 
 function listCities(event) {
     event.preventDefault();
     console.log(event);
-    var list = cityInput.Value;
+    var list = "cityInput.value";
     cityListEl.textContent = list;
 }
-// searchBtnEl.addEventListener('submit', showCityList);
+searchBtnEl.addEventListener('click', showCityList);
+
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    var searchInputVal = document.querySelector('#city-input').value;
+
+
+    if (!searchInputVal) {
+        console.error('You need a search input value!');
+        return;
+    }
+
+    var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+    location.assign(queryString);
+}
+
+weatherFormEl.addEventListener('submit', handleSearchFormSubmit);
